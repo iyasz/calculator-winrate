fb50077766255ddfd0ca7c596a6b894c4588b2e0
Yasz Avellia <zakamaragames@gmail.com>
2023-02-27 20:19:51 +0700

    menambah code login dan logout di auth controller

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class authController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authLogin(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if(Auth::attempt($validated)){
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'email salah'
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/auth/login');
+    }
 }
